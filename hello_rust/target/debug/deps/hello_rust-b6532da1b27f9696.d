482801fa613f03a43a41e40e23b88df3d5f3c324
Fredy Rodriguez <proyectos@fredyrn.com>
2024-08-20 16:57:36 -0500

    Created hello_rust program, it's just a hello world prompt.

---
+++ hello_rust/target/debug/deps/hello_rust-b6532da1b27f9696.d
@@ -0,0 +1,5 @@
+/media/data/.projects/RUST_from_scratch/hello_rust/target/debug/deps/libhello_rust-b6532da1b27f9696.rmeta: src/main.rs
+
+/media/data/.projects/RUST_from_scratch/hello_rust/target/debug/deps/hello_rust-b6532da1b27f9696.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/hello_rust.d
@@ -0,0 +1,1 @@
+/media/data/.projects/RUST_from_scratch/hello_rust/target/debug/hello_rust: /media/data/.projects/RUST_from_scratch/hello_rust/src/main.rs
