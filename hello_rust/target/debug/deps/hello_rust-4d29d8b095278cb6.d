60686beb5cc34a4d5cde0843757c4017b836af8e
Fredy Rodriguez <proyectos@fredyrn.com>
2024-08-20 22:08:08 -0500

    Compiled hello world program

---
+++ hello_rust/target/debug/deps/hello_rust-4d29d8b095278cb6.d
@@ -0,0 +1,5 @@
+/media/data/.projects/RUST_from_scratch/hello_rust/target/debug/deps/libhello_rust-4d29d8b095278cb6.rmeta: src/main.rs
+
+/media/data/.projects/RUST_from_scratch/hello_rust/target/debug/deps/hello_rust-4d29d8b095278cb6.d: src/main.rs
+
+src/main.rs:
---
+++ hello_rust/target/debug/deps/hello_rust-570c5ea13f90aada.d
@@ -0,0 +1,5 @@
+/media/data/.projects/RUST_from_scratch/hello_rust/target/debug/deps/hello_rust-570c5ea13f90aada: src/main.rs
+
+/media/data/.projects/RUST_from_scratch/hello_rust/target/debug/deps/hello_rust-570c5ea13f90aada.d: src/main.rs
+
+src/main.rs:
