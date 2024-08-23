413220510a5c8f1e7f6afac4b8d0141ac533631c
Fredy Rodriguez <proyectos@fredyrn.com>
2024-08-23 15:15:01 -0500

    Added <<collections>> project, it's to study tuples and strucks in RUST

---
+++ collections/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("Collections: tuples");
+    println!("*-------------------*");
+    let tuple_a = ('W', 5i32, true, "WORD");
+    
+}
---
+++ collections/target/debug/deps/collections-89e6ee487859d0f4.d
@@ -0,0 +1,5 @@
+/media/data/.projects/RUST_from_scratch/collections/target/debug/deps/collections-89e6ee487859d0f4: src/main.rs
+
+/media/data/.projects/RUST_from_scratch/collections/target/debug/deps/collections-89e6ee487859d0f4.d: src/main.rs
+
+src/main.rs:
