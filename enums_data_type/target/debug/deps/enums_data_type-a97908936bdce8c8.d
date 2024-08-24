f509b73bc1702bf2f4f3b35ea205ef9873198249
Fredy Rodriguez <proyectos@fredyrn.com>
2024-08-24 18:27:13 -0500

    Seeing Enums variants.

---
+++ enums_data_type/target/debug/deps/enums_data_type-a97908936bdce8c8.d
@@ -0,0 +1,5 @@
+/media/data/.projects/RUST_from_scratch/enums_data_type/target/debug/deps/enums_data_type-a97908936bdce8c8: src/main.rs
+
+/media/data/.projects/RUST_from_scratch/enums_data_type/target/debug/deps/enums_data_type-a97908936bdce8c8.d: src/main.rs
+
+src/main.rs:
---
+++ enums_data_type/target/debug/enums_data_type.d
@@ -0,0 +1,1 @@
+/media/data/.projects/RUST_from_scratch/enums_data_type/target/debug/enums_data_type: /media/data/.projects/RUST_from_scratch/enums_data_type/src/main.rs
