03c91c69938f445f047423b8c335896656f52ab4
Fredy Rodriguez <proyectos@fredyrn.com>
2024-12-21 02:54:40 -0500

    Updated fibonacci calculator, now use a loop to calculate it.

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,12 +1,23 @@
-fn fibo(n: u32) -> u32 {
-    if n < 2 {
-        return n;
-    } else {
-        return fibo(n - 1) + fibo(n - 2);
+// fn fibo(n: u32) -> u32 {
+//     if n < 2 {
+//         return n;
+//     } else {
+//         return fibo(n - 1) + fibo(n - 2);
+//     }
+// }
+
+fn fibo(n: u32) -> u128 {
+    let mut a: u128 = 0;
+    let mut b: u128 = 1;
+    for _ in 0..n {
+        let temp = a;
+        a = b;
+        b = temp + b;
     }
+    a
 }
 
 fn main() {
-    let n: u32 = 300;
+    let n: u32 = 50;
     println!("Fibonacci({n}) = {}", fibo(n));
 }
