4affb2236e27fd68f5a5d99742570ce87685a4fd
Fredy Rodriguez <proyectos@fredyrn.com>
2024-12-22 00:02:48 -0500

    Updated fibobacci project, now accept big numbers.

--- projects/fibonacci/src/main.rs
+++ projects/fibonacci/src/main.rs
@@ -1,3 +1,6 @@
+use num_bigint::BigUint;
+use num_traits::One;
+
 // fn fibo(n: u32) -> u32 {
 //     if n < 2 {
 //         return n;
@@ -6,18 +9,29 @@
 //     }
 // }
 
-fn fibo(n: u32) -> u128 {
-    let mut a: u128 = 0;
-    let mut b: u128 = 1;
+// fn fibo(n: u32) -> u128 {
+//     let mut a: u128 = 0;
+//     let mut b: u128 = 1;
+//     for _ in 0..n {
+//         let temp = a;
+//         a = b;
+//         b = temp + b;
+//     }
+//     a
+// }
+
+fn fibo(n: usize) -> BigUint {
+    let mut f0 = BigUint::ZERO;
+    let mut f1 = BigUint::one();
     for _ in 0..n {
-        let temp = a;
-        a = b;
-        b = temp + b;
+        let f2 = f0 + &f1;
+        f0 = f1;
+        f1 = f2;
     }
-    a
+    f0
 }
 
 fn main() {
-    let n: u32 = 50;
+    let n: usize = 3000;
     println!("Fibonacci({n}) = {}", fibo(n));
 }
